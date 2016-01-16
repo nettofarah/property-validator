f7de72bf397b5f34ffed8b1f3f1d29dc2abeb09c
Netto Farah <nettofarah@gmail.com>
2016-01-16 13:16:59 -0800

    separate validations from validators

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./lib/request_validator')
+var merge = require('lodash/merge');
+
+var requestValidator = require('./lib/request_validator');
+var validations = require('./lib/validations');
+
+module.exports = merge(requestValidator, validations);
