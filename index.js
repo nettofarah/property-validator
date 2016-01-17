657b87d9ef8441613e003c3a6fa9c12a36afd4ed
Netto Farah <nettofarah@gmail.com>
2016-01-16 19:38:54 -0800

    add assertions to global namespace

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var merge = require('lodash/merge');
 
 var requestValidator = require('./lib/request_validator');
 var validations = require('./lib/validations');
+var assertions = require('./lib/assertions');
 
-module.exports = merge(requestValidator, validations);
+module.exports = merge(requestValidator, validations, assertions);
