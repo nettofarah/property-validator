867447e3711c9a0668bd409b5d81cba97ba4dc17
Netto Farah <nettofarah@gmail.com>
2016-01-17 19:13:57 -0800

    add happy path test case

--- test/middleware_test.js
+++ test/middleware_test.js
@@ -10,51 +10,54 @@ var email = require('../lib/validations').email;
 
 describe('Validation Middleware', function() {
 
-  describe('intercepting invalid requests', function() {
-    var app;
+  var app;
 
-    app = express();
-    app.get('/hello', function(req, res){
-      assertAll(req, [
-        presence('username'),
-        email('email_address')
-      ]);
+  app = express();
+  app.get('/hello', function(req, res){
+    assertAll(req, [
+      presence('username'),
+      email('email_address')
+    ]);
 
-      res.status(200).json({ name: 'netto' });
-    });
+    res.status(200).json({ name: req.query.username });
+  });
 
-    app.use(middleware);
+  app.use(middleware);
 
-    it('sends the appropriate response code', function(done) {
-      var response = request(app).get('/hello');
-      response.expect(422, done);
-    });
+  it('sends the appropriate response code', function(done) {
+    var response = request(app).get('/hello');
+    response.expect(422, done);
+  });
 
-    it('sends all errors back in the response', function(done) {
-      var response = request(app).
-        get('/hello').
+  it('sends all errors back in the response', function(done) {
+    var response = request(app).
+      get('/hello').
         set('Accept', 'application/json');
 
 
-      var expectedResponse = {
-        errors: [
-          '"username" required',
-          '"email_address" should look like an email address',
-        ]
-      };
+        var expectedResponse = {
+          errors: [
+            '"username" required',
+            '"email_address" should look like an email address',
+          ]
+        };
 
-      response.expect('Content-type', /json/);
-      response.expect(422, expectedResponse, done);
-    });
+        response.expect('Content-type', /json/);
+        response.expect(422, expectedResponse, done);
+  });
 
-    it('respects the content type', function(done) {
-      var response = request(app).get('/hello');
-      response.expect('Content-type', 'text/plain; charset=utf-8');
+  it('respects the content type', function(done) {
+    var response = request(app).get('/hello');
+    response.expect('Content-type', 'text/plain; charset=utf-8');
 
-      var expectedResponse = 'Invalid Request: "username" required, "email_address" should look like an email address';
+    var expectedResponse = 'Invalid Request: "username" required, "email_address" should look like an email address';
 
-      response.expect(422, expectedResponse, done);
-    });
+    response.expect(422, expectedResponse, done);
+  });
+
+  it('does not intercept valid requests', function(done) {
+    var response = request(app).get('/hello?username=netto&email_address=nettofarah@gmail.com');
+    response.expect(200, { name: 'netto' }, done);
   });
 });
 
