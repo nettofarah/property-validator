19c2a2dfae279022c7e2e8db618ae3554cc57af7
nettofarah <nettofarah@gmail.com>
2016-01-28 22:57:32 -0800

    add es6 example

---
+++ examples/examples.es6.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+import {
+  assertAll,
+  assertBody,
+  assertQuery,
+  presence,
+  email,
+  assertMiddleware
+} from '../index';
+
+const app = express();
+
+app.get('/any-param', (req, res) => {
+  assertAll(req, [
+    presence('username'),
+    email('email_address')
+  ]);
+
+  res.status(200).json({ name: req.query.username });
+});
+
+
+app.post('/body-params', (req, res) => {
+  assertBody(req, [
+    presence('username'),
+    email('email_address')
+  ]);
+
+  res.status(200).json({ name: req.query.username });
+});
+
+
+app.get('/query-params', (req, res) => {
+  assertQuery(req, [
+    presence('username'),
+    email('email_address')
+  ]);
+
+  res.status(200).json({ name: req.query.username });
+});
+
+app.use(assertMiddleware);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
